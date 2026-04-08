1fbc7b58444a3ba22bed250fc59ebae3bb557174
PradaDipa <putu.pradadipa@gmail.com>
2026-04-08 21:10:07 +0800

    Delete file

--- generate_pass.py
+++
@@ -1,9 +0,0 @@
-import streamlit_authenticator as stauth
-
-# Cara baru untuk Streamlit-Authenticator versi terbaru
-password_asli = '*rumahningrat112#'  # Ganti dengan password asli yang ingin di-hash
-
-# Gunakan method .hash() secara langsung untuk satu password
-hashed_password = stauth.Hasher.hash(password_asli)
-
-print(hashed_password)
